ad9efa19811e16efc7fd66095cf2cfcaf1115af8
Somesh sah <somesh.sah@gmail.com>
2023-05-08 22:42:16 +0530

    fixed.

--- com/learn/basics/palindromcode/StringPalindrom.java
+++ com/learn/basics/palindromcode/StringPalindrom.java
@@ -14,23 +14,27 @@ public class StringPalindrom {
         return false;
     }
 
-//    public static boolean isPalindromWayTwo(String original){
-//        boolean isPalindrom = false;
-//        for(int i=0, j= original.length()-1 ; i<= original.length()/2 ; i++,j--){
-//            if(original.charAt(i) == original.charAt(j))
-//                isPalindrom =true;
-//        }
-//        return isPalindrom;
-//    }
+    private static boolean isPalindromWayTwo(String original){
+        int len = original.length();
+        boolean answer = true ;
+        for(int i = 0 ; i<= len/2 ; i++){
+            if(original.charAt(i) != original.charAt((len-1) -i)){
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(isPalindromWayOne("asdfxrtdhedsxfdsa"));
         System.out.println(isPalindromWayOne("asdffdsa"));
         System.out.println(isPalindromWayOne("asdfxfdsa"));
-//        System.out.println("===============");
-//        System.out.println(isPalindromWayTwo("asdfxrtdhedsxfdsa"));
-//        System.out.println(isPalindromWayTwo("asdffdsa"));
-//        System.out.println(isPalindromWayTwo("asdfxfdsa"));
+        System.out.println("===============");
+        System.out.println(isPalindromWayTwo("asdfxrtdhedsxfdsa"));
+        System.out.println(isPalindromWayTwo("asdffdsa"));
+        System.out.println(isPalindromWayTwo("asdfxfdsa"));
     }
 
 
