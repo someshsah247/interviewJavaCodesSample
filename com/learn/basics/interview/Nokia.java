6cd300793d0bffb68d4becb275065c55dfba388f
Somesh sah <somesh.sah@gmail.com>
2024-04-05 01:02:14 +0530

    added..some more code.

---
+++ com/learn/basics/interview/Nokia.java
@@ -0,0 +1,154 @@
+package com.learn.basics.interview;
+
+// Maza aaye  --- full too coding
+
+public class Nokia {
+}
+/*
+oops concept explanation with code
+
+class Emp{
+
+	private String name;
+	private Long empId;
+
+	public String getName(){
+		retuen this.name ;
+	}
+
+	public void setId(Long  id){
+
+		this.name = "emp"+id;
+	}
+
+		public String getName(){
+		retuen this.name ;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+
+	// Inheritance
+
+
+	Base ---> Derived class
+
+	Single
+	multilevel
+	multiple X
+	hybrid
+	hararical
+
+	A ---> B ---> C ---> D, E
+
+
+	Polymorphise
+
+
+	Orver loading and  over ridding
+
+	A{
+
+	void print(){
+	sop("A");
+	}
+
+	class B extends A{
+
+		@Override
+		void print(){
+		sop("B");
+		}
+	}
+
+
+	A{
+
+	void print(int a, String s){
+	sop(a);
+
+	void print(String s, int a){
+	sop(s);
+
+	}
+
+
+	Abstraction
+
+	public Product findProd( Long id){
+
+		Product p = repository.finfById(id);
+		if(pf
+		p.getName();
+
+Optione[]
+
+		Emp l = (Emp) Person()
+
+	}
+
+	// cuncurent modification exception code
+	List<Integers> list = new ArrayList();
+	//List.of(1,2,3,4,5,68,9,10);
+	list.add();
+
+
+	for(int i = 0 ; l<=list.size() ; i++){
+			list.add();
+	}
+
+
+	Hash Set -- Hash Map Similarities  ?
+
+
+	class A {
+
+	String str ;
+
+	A(String s ){
+		this.str = s;
+	}
+
+	public String toString(){
+		reteun str;
+	}
+
+	equals & hashcode implementation bassed on above scenario
+
+	public boolean equals(Object o){
+
+	if(o instanceOf A )
+		if(A != null){
+			return this.o. o ;
+
+	}
+
+	int hasCode(){
+		retern
+	}
+
+
+	}
+
+	HashSet<Object> set = new HashSet()<>;
+
+	set.add("test");
+	A a = new A("test");
+	set.add(a);
+
+	// test, A@6t3935
+	// test, test
+
+	// he want only 1 answer "test" -- we need to overider hash code and equals method -- how to do that ?
+
+
+
+
+
+
+
+}
+
+ */
